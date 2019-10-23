f41600005bdc3764272a472bbcbed8279046c014
ypstream <yohanes3ddesign@gmail.com>
2019-10-24 01:20:28 +0700

    add new home page style

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+
+/*  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      typeSpeed: 100,
+      loop: true,
+    });
+  }); */
+
+  new Typed('#typed',{
+  	strings : ['Faster', 'Eficience', 'Cheapest'],
+  	typeSpeed : 80,
+  	delaySpeed : 1500,
+  	loop : true
+  })
